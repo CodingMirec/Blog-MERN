f0309eb5fcd1245630777b9b3a0650c2c2e4161c
CodingMirec <m.sykora01@gmail.com>
2021-11-22 17:32:02 +0100

    config set up

---
+++ client/src/config/config.ts
@@ -0,0 +1,16 @@
+const config = {
+    firebase: {
+        apiKey: 'AIzaSyCa1rQy6CCnmkJJqKOKfidDtsTuXyPbI8E',
+        authDomain: 'blog-mern-type.firebaseapp.com',
+        projectId: 'blog-mern-type',
+        storageBucket: 'blog-mern-type.appspot.com',
+        messagingSenderId: '239774589165',
+        appId: '1:239774589165:web:c35ae422cbefebcd60ace4',
+        measurementId: 'G-XCZ3WK1VN8'
+    },
+    server: {
+        url: 'http://localhost:1337'
+    }
+};
+
+export default config;
