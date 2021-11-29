30562e076cb5eb64c8c1165a790a4e526f740c38
CodingMirec <m.sykora01@gmail.com>
2021-11-29 17:26:57 +0100

    application props

--- client/src/application.tsx
+++ client/src/application.tsx
@@ -1,10 +1,57 @@
-import React from 'react';
+import React, { useEffect, useReducer, useState } from 'react';
 import { Switch, RouteChildrenProps, Route } from 'react-router-dom';
 import routes from './config/routes';
+import { initialUserState, userReducer } from './contexts/user';
 
 export interface IApplicationProps {}
 
 const Application: React.FunctionComponent<IApplicationProps> = (props) => {
+    const [userState, userDispatch] = useReducer(userReducer, initialUserState);
+    const [loading, setLoading] = useState<boolean>(true);
+
+    // Used for Debugging
+    const [authStage, setAuthStage] = useState<string>(
+        'Checking localstorage ...'
+    );
+
+    useEffect(() => {
+        setTimeout(() => {
+            CheckLocalStorageForCredentials();
+        }, 1000);
+    }, []);
+
+    /*Check to see if we have a token
+    If we do verify it with backend then
+    If not, we are logged out initially
+    */
+    const CheckLocalStorageForCredentials = () => {
+        setAuthStage('Checking credentials ...');
+
+        const fire_token = localStorage.getItem('fire_token');
+
+        if (fire_token === null) {
+            userDispatch({ type: 'logout', payload: initialUserState });
+            setAuthStage('No credentials found');
+            setTimeout(() => {
+                setLoading(false);
+            }, 1000);
+        } else {
+            // Validate with backend
+            setAuthStage('No credentials found, validating ...');
+            setTimeout(() => {
+                setLoading(false);
+            }, 1000);
+        }
+    };
+
+    const userContextValues = {
+        userState,
+        userDispatch
+    };
+
+    if (loading) {
+    }
+
     return (
         <Switch>
             {routes.map((route, index) => {
--- client/src/config/firebase.ts
+++ client/src/config/firebase.ts
@@ -1,6 +1,6 @@
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
-import 'firebase/firestore';
+import 'firebase/compat/firestore';
 import config from './config';
 
 const Firebase = firebase.initializeApp(config.firebase);
